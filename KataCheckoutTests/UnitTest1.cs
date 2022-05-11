aa7a556515b51b3fe7bba8504ce73b75aba52d66
kunistvan <ikunki@outlook.com>
2022-05-11 18:29:13 +0100

    2nd commit

--- KataCheckoutTests/UnitTest1.cs
+++ KataCheckoutTests/UnitTest1.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Xunit;
+using KataCheckoutService.Models;
+using KataCheckoutService.Interfaces;
 
 namespace KataCheckoutTests
 {
-    public class UnitTest1
+    public class KataCheckoutServiceTests
     {
-        [Fact]
-        public void Test1()
+        private static IKataCheckoutService KataService(IDictionary<string, decimal> prices, IDictionary<string, IEnumerable<Discount>> discounts)
         {
-
+            return new KataCheckoutService(prices, discounts);
         }
+
+        #region ScanItem()
+
+        [Fact]
+        public void ScanItem_notFoundException()
+        {
+            // Arrange
+            var prices = MockedPrices();
+            var discounts = MockedDiscounts();
+            var service = KataService(prices, discounts);
+
+            // Act & Assert
+            Assert.Throws<SKUNotFoundException>(() => service.ScanItem("E", 1));
+        }
+
+        [Theory]
+        public void ScanItem_qtyZeroOrLessException(int quantity)
+        {
+            // Arrange
+            var prices = MockedPrices();
+            var discounts = MockedDiscounts();
+            var service = KataService(prices, discounts);
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => service.ScanItem("A", quantity));
+        }
+
+        #endregion
     }
 }
